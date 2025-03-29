c691ff4b4c14429d00aef9fd2a394a60c244a461
pankaj kumar <pankaj.syal1@gmail.com>
2025-03-29 11:49:03 +0530

    change projects data

--- src/components/projects/ProjectsLinks.jsx
+++ src/components/projects/ProjectsLinks.jsx
@@ -1,31 +1,38 @@
 import PropTypes from "prop-types";
 import { Link } from "react-router-dom";
-import { projects } from "../../data/projects";
-import { generateRandomColor } from "../../utils/common-functon"
+import { allProjects } from "../../data/projects";
+import { generateRandomColor } from "../../utils/common-functon";
 
-const ProjectLinks = () => (
-  <>
-    <div className="bg-gray-800 p-4">
-      <h3 className="text-lg mb-2">{"Beginer Projects"}</h3>
-      <ul className="flex flex-wrap gap-4 text-white">
-        {projects.map((project, index) => (
-          <li
-            key={index}
-            className="px-2 py-1"
-            style={{ backgroundColor: generateRandomColor() }}
-          >
-            <Link
-              to={project.path}
-              className="text-sm font-medium hover:text-black transition duration-200"
-            >
-              {project.title}
-            </Link>
-          </li>
-        ))}
-      </ul>
-    </div>
-  </>
-);
+const ProjectLinks = () => {
+  return (
+    <>
+      {allProjects.map((res, index) => (
+        <div className="bg-gray-800 p-4" key={index}>
+          <h3 className="text-lg mb-2">{res.type}</h3>
+          <ul className="flex flex-wrap gap-4 text-white">
+            {res.projects.map((project, idx) => (
+              <li
+                key={idx}
+                className="px-2 py-1"
+                style={{ backgroundColor: generateRandomColor() }}
+              >
+                <Link
+                  to={project.path}
+                  className="text-sm font-medium hover:text-black transition duration-200"
+                >
+                  {project.title}
+                </Link>
+              </li>
+            ))}
+          </ul>
+        </div>
+      ))}
+    </>
+  );
+};
 
-ProjectLinks.propTypes = { title: PropTypes.string };
+ProjectLinks.propTypes = {
+  title: PropTypes.string,
+};
+
 export default ProjectLinks;
--- src/data/projects.jsx
+++ src/data/projects.jsx
@@ -1,12 +1,22 @@
-export const projects = [
-  { path: "/counter", title: "Counter" },
-  { path: "/todos", title: "Todos" },
-  { path: "/meals", title: "Meals" },
-  { path: "/calculator", title: "Calculator" },
-  { path: "/theme-toggler", title: "Toggle Theme" },
-  { path: "/search-bar", title: "Searchbar" },
-  { path: "/testimonials", title: "Testimonials" },
-  { path: "/accordion", title: "Accordion App" },
-  { path: "/form-validation", title: "Form Validation" },
-  { path: "/advanced-filter", title: "Advance Filter" },
-];
+export const allProjects = [
+  {
+    type: "Beginer Projects",
+    projects: [
+      { path: "/counter", title: "Counter" },
+      { path: "/todos", title: "Todos" },
+      { path: "/meals", title: "Meals" },
+      { path: "/calculator", title: "Calculator" },
+      { path: "/theme-toggler", title: "Toggle Theme" },
+      { path: "/search-bar", title: "Searchbar" },
+      { path: "/testimonials", title: "Testimonials" },
+      { path: "/accordion", title: "Accordion App" },
+      { path: "/form-validation", title: "Form Validation" },
+    ]
+  },
+  {
+    type: "Intermediate Projects",
+    projects: [
+      { path: "/advanced-filter", title: "Ecommerce Filter" },
+    ]
+  }
+]
